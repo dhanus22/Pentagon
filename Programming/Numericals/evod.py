06b5f6b0f5c88c95af7df294eb695daff5778ab7
dhanus22 <smsaidhanush153@gmail.com>
2025-07-24 21:54:33 +0530

    init

--- Programming/Numericals/evod.py
+++ Programming/Numericals/evod.py
@@ -2,7 +2,7 @@ def evenodd(n):
     return (n % 2 == 0)
 
 n = int(input("enter num"))
-flag = evenodd(10)#Flag is a standard variable name to hold a Boolean value
+flag = evenodd(n)#Flag is a standard variable name to hold a Boolean value
 
 if flag:
     print(f"{n} is even")
