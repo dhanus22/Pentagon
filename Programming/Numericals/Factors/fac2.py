95559c2b0a3cca0593ab86f7215155feff66dc59
dhanus22 <smsaidhanush153@gmail.com>
2025-07-31 20:36:53 +0530

    init

--- Programming/Numericals/Factors/fac2.py
+++ Programming/Numericals/Factors/fac2.py
@@ -1,11 +1,19 @@
 def factors(n):
-    for i in range(1,n+1):
-        if(n % i == 0):
-            print(i,end=" ")
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i, end=" ")
+            count += 1
+    return count
 
 sr = int(input("Enter start range:"))
 er = int(input("Ente end range:"))
 if (sr > er):
     print("Invalid input")
+for i in range(sr, er + 1):
+    print(f"\nFactors of {i} are: ", end="")
+    res = factors(i)
+    print(f"\nThe count is : {res}")
 
+    
 
